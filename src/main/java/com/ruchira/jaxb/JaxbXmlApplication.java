180b2ed854ee1526fba5d1c30af960ce1f4dd3d1
Ruchira Madhushan Rajapaksha <52396694+LordMaduz@users.noreply.github.com>
2024-07-18 00:05:01 +0800

    Add files via upload

---
+++ src/main/java/com/ruchira/jaxb/JaxbXmlApplication.java
@@ -0,0 +1,13 @@
+package com.ruchira.jaxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxbXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxbXmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruchira/jaxb/JaxbXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruchira.jaxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxbXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
